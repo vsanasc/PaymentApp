3e24e5d8205e7c49efc27624f9e36da9f74eb8d6
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-01-11 00:54:58 -0300

    Add initial installaments model

---
+++ PaymentApp/Models/InstallmentModel.m
@@ -0,0 +1,13 @@
+//
+//  InstallmentModel.m
+//  PaymentApp
+//
+//  Created by Vitor Santos do Nascimento on 1/11/18.
+//
+
+#import "InstallmentModel.h"
+
+@implementation InstallmentModel
+
+
+@end
