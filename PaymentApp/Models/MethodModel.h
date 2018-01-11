f5059f2d6a0f0874b5bfad6c9c9cda72962712d9
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-01-11 00:55:11 -0300

    Add initial method model

---
+++ PaymentApp/Models/MethodModel.h
@@ -0,0 +1,16 @@
+//
+//  MethodModel.h
+//  PaymentApp
+//
+//  Created by Vitor Santos do Nascimento on 1/11/18.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MethodModel : NSObject
+
+@property (strong,nonatomic) NSString *id;
+@property (strong,nonatomic) NSString *name;
+@property (strong,nonatomic) NSString *logo;
+
+@end
