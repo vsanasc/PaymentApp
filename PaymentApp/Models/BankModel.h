f9e66731974d4ca1f5c59e00828f9278e3bb3b62
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-01-11 00:54:42 -0300

    Add initial Bank model

---
+++ PaymentApp/Models/BankModel.h
@@ -0,0 +1,16 @@
+//
+//  BankModel.h
+//  PaymentApp
+//
+//  Created by Vitor Santos do Nascimento on 1/11/18.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BankModel : NSObject
+
+@property (strong,nonatomic) NSString *id;
+@property (strong,nonatomic) NSString *name;
+@property (strong,nonatomic) NSString *logo;
+
+@end
