56205cc4f2e9df9d42f4e2de00fb5b683af41ab6
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-01-14 03:42:59 -0300

    Add NSLayoutContraint description category

---
+++ PaymentApp/Healpers/NSLayoutConstraint+Description.h
@@ -0,0 +1,13 @@
+//
+//  NSLayoutConstraint+Description.h
+//  PaymentApp
+//
+//  Created by Vitor Santos do Nascimento on 1/13/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSLayoutConstraint (Description)
+
+
+@end
