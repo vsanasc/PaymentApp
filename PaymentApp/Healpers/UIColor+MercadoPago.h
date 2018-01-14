877025f05116ce0902d7a03b0d66d4c5c16e1646
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-01-14 01:38:06 -0300

    Add uicolor category with category

---
+++ PaymentApp/Healpers/UIColor+MercadoPago.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+MercadoPago.h
+//  PaymentApp
+//
+//  Created by Vitor Santos do Nascimento on 1/13/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (MercadoPago)
+
++(UIColor *)mpLightBlue;
++(UIColor *)mpBlue;
+
+@end
---
+++ PaymentApp/Healpers/UIColor+MercadoPago.m
@@ -0,0 +1,19 @@
+//
+//  UIColor+MercadoPago.m
+//  PaymentApp
+//
+//  Created by Vitor Santos do Nascimento on 1/13/18.
+//
+
+#import "UIColor+MercadoPago.h"
+
+@implementation UIColor (MercadoPago)
+
++(UIColor *)mpLightBlue{
+	return [UIColor colorWithRed:0.00 green:0.62 blue:0.89 alpha:1.0];
+}
++(UIColor *)mpBlue{
+	return [UIColor colorWithRed:0.00 green:0.14 blue:0.55 alpha:1.0];
+}
+
+@end
