ace4c98f034604d86e7032ad0a9f3db74535f673
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-01-14 21:12:24 -0300

    Add transaction model

---
+++ PaymentApp/Models/TransactionModel.m
@@ -0,0 +1,12 @@
+//
+//  TransactionModel.m
+//  PaymentApp
+//
+//  Created by Vitor Santos do Nascimento on 1/14/18.
+//
+
+#import "TransactionModel.h"
+
+@implementation TransactionModel
+
+@end
